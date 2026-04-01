776b9fbb1245214755fce0da6a5ea6200abf8b1e
Muhindhar <muhindharsv27@gmail.com>
2026-04-01 12:31:02 +0530

    latest

---
+++ src/main/java/test_package/Flipkart.java
@@ -0,0 +1,26 @@
+package test_package;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Flipkart {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.flipkart.com/");
+
+        String title = driver.getTitle();
+        System.out.println(title);
+        System.out.println("Title Length : " + title.length());
+        String url = driver.getCurrentUrl();
+        String exp = "https://www.flipkart.com/";
+        if (exp.equals(url)) {
+            System.out.println("It is correct URL");
+        } else {
+            System.out.println("Not a correct URL");
+        }
+        String page = driver.getPageSource();
+        System.out.println("PAGE LENGTH : " + page.length());
+        driver.quit();
+    }
+}
